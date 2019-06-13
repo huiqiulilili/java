8fae56707c6e59add5ad8da3ab5ddc2add4aa38c
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-06-13 12:01:42 +0800

    lx 2019-6-13

---
+++ 1906/11.java
@@ -0,0 +1,43 @@
+class ListNode{
+	int val;
+	ListNode next;
+	public ListNode(int val){
+		this.val = val;
+	}
+}
+
+public class Interview2{
+	
+	public ListNode deleteDuplication(ListNode pHead){
+		
+		if (pHead == null) {
+			return null;
+		}
+		ListNode p1 = pHead;
+		ListNode p2 = pHead.next;
+		ListNode prev = null;
+		
+		while(p2 != null){
+			if(p1.val != p2.val){
+				//如果两个不相等
+				prev = p1;
+			}else{
+				//如果两个相等
+				while(p2 != null && p2.val == p1.val){
+					p2 = p2.next;
+				}
+				if(prev == null){
+					pHead = p2;
+				}else{
+				prev.next = p2;
+				}
+				
+			}
+				p1 = p2;
+				if(p2 != null){
+					p2 = p2.next;
+				}
+		}
+	}
+	return fake.next;
+}
