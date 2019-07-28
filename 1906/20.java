1bb48f9c78f198763b022b7a830e2c6ad4e0fa4f
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-07-28 20:38:40 +0800

    Create 20.java

---
+++ 1906/20.java
@@ -0,0 +1,18 @@
+ class Hero{
+  String name;
+  float hp;
+  float armor;
+  int moveSpeed;
+}
+
+public class Practice3 {
+    public static void main(String[] args) {
+        Hero garen = new Hero();
+        garen.name = "盖伦";
+        garen.hp = 616f;
+        garen.armor = 14f;
+        garen.moveSpeed = 350;
+
+        
+    }
+}
