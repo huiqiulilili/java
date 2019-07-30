48dea97341ae38a5d8a46c569b006cd442768d88
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-07-30 19:06:24 +0800

    Create 21.java

---
+++ 1906/21.java
@@ -0,0 +1,8 @@
+
+public class Practice9 {
+    public static void main(String[] args) {
+        System.out.println("byte 型的范围");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
