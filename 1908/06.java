6f1e13b7e307cac0ab0b56fad5a66ed33846e680
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-08-21 16:57:56 +0800

    Create 06.java

---
+++ 1908/06.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+//        List list = new MyArrayList();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        list.add(1,20);
+//        list.remove(1);
+//
+//        list.set(0,10);
+//        System.out.println(list);
+//        System.out.println(list.isEmpty());
+
+
+        List list = new MyLindedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+//        list.add(1,20);
+//        list.remove(1);
+
+//        list.set(0,10);
+        System.out.println(list);
+//        System.out.println(list.isEmpty());
+    }
+
+}
